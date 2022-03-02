9b32409d58fd66ed7bcebf989fc2f643055dc65a
Stelianos Langaditis <slangaditis96@gmail.com>
2022-03-02 15:25:09 -0800

    files created

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,18 @@
+// AppRouter
+// Development Components
+// import { BrowserRouter } from 'react-router-dom';
+
+
+
+
+function AppRouter() {
+  return (
+  
+    <div className="wrapper">
+
+    </div>
+   
+  );
+}
+
+export default AppRouter;
