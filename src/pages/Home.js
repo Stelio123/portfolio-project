1cf353d510a4b069434c7b475339cad1e51bb8e7
Stelianos Langaditis <slangaditis96@gmail.com>
2022-03-08 22:06:39 -0800

    added video(not working)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,26 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 
 function Home() {
+
+//   $(window).on('load', addNoise);
+
+// function addNoise() {
+//   $('.loader').text('Bring in \'da noise!');
+//   $('.noise-wrapper').css('opacity',1);
+// }
   return (
+    <>
+    <video autoplay loop id="myVideo">
+      <source src='../assets/videos/background-video.mp4' type="video/mp4"/>
+    </video>
     <div className='home-container'>
       <h1>Stelio Langaditis</h1>
-      <h1>Welcome</h1>
       <p>Front-end web developer + designer</p>
       <div className='btn-container'>
-      <Link to='work'><button className='btn'>click me</button></Link>
+        <Link to='work'><button className='btn'>click me</button></Link>
       </div>
     </div>
+    </>
   )
 }
 
