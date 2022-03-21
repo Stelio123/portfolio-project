b6d8fcefa30a90f242e7887d7c574b55920fb5fe
Stelianos Langaditis <slangaditis96@gmail.com>
2022-03-21 13:42:15 -0700

    added rel tags to a tags in projectData

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -6,8 +6,8 @@ export const ProjectData = [
         summary: 'SNOS Movies is a single page web application built in React. SNOS Movies utilizes REST API to allow users to find up-to-date information about popular, top rated, now playing, and upcoming movies. Users are also able to add movies to their favourites.',
         tools: 'React, Sass, REST API, GitHub',
         links:[
-        <a href="https://github.com/" target='_blank' alt='github'>GitHub</a>, 
-        <a href='https://steliocodes.com/snos-movies/' target='_blank' alt='movie databe wesite' >Live Site</a>],
+        <a href="https://github.com/" rel='noreffer noopener'  target='_blank' alt='github'>GitHub</a>, 
+        <a href='https://steliocodes.com/snos-movies/' rel='noreffer noopener'target='_blank' alt='movie databe wesite' >Live Site</a>],
         mockTitle: 'Mock Up',
         projectImage:[
         <img src={require('../assets/images/mdb.png')} alt='movie database homepage.'></img>, 
@@ -30,8 +30,8 @@ export const ProjectData = [
         image:require('../assets/images/minesweeper.png'),
         summary: 'Developed and designed a mine sweeper clone using vanilla JavaScript and jQuery. All styling was done using CSS. With this project being my first time implementing my skills in JavaScript to create a web application, I am very proud of the outcome. ',
         tools: 'JavaScript, jQuery, HTML, CSS',
-        links: [<a href="https://github.com/" target='_blank' alt='github'>GitHub</a>, 
-                <a href="https://steliocodes.com/minesweeper/" target='_blank' alt='github'>Live Site</a>],
+        links: [<a href="https://github.com/" rel='noreffer noopener' target='_blank' alt='github'>GitHub</a>, 
+                <a href="https://steliocodes.com/minesweeper/" rel='noreffer noopener' target='_blank' alt='github'>Live Site</a>],
         projectImage: [<img src={require('../assets/images/minesweeper-home.png')} alt= 'minesweeper home'></img>, 
                        <img src={require('../assets/images/main-screen.png')} alt='main screen'></img>],
         functionTitle:'Functionality',
@@ -43,7 +43,7 @@ export const ProjectData = [
         image: require('../assets/images/calculator.png'),
         summary: 'Built a basic calculator for fun using React and CSS.',
         tools: 'React, CSS',
-        links: <a href="https://steliocodes.com/minesweeper/" target='_blank' alt='live site'>Live Site</a>,
+        links: <a href="https://steliocodes.com/minesweeper/" rel='noreffer noopener' target='_blank' alt='live site'>Live Site</a>,
         slug: 'calculator'
 
     },
@@ -52,8 +52,8 @@ export const ProjectData = [
         image: require('../assets/images/html.png'),
         summary: 'Developed a fully responsive replica restaurant website in a team of two using HTML, SASS and GitHub.', 
         tools: 'HTML, SASS, GitHub',
-        links: [<a href="https://github.com/" target='_blank' alt='github'>GitHub</a>, 
-                <a href="https://steliocodes.com/minesweeper/" target='_blank' alt='github'>Live Site</a>],
+        links: [<a href="https://github.com/"  rel='noreffer noopener' target='_blank' alt='github'>GitHub</a>, 
+                <a href="https://steliocodes.com/minesweeper/" rel='noreffer noopener' target='_blank' alt='github'>Live Site</a>],
         slug: 'foodexplorer'
 
     },
@@ -62,8 +62,8 @@ export const ProjectData = [
         image: require('../assets/images/woo.png'),
         summary: 'Developed an E-commerce website for a non-existing company called "Sacred Tree Massage Therapy". Working in a team of four, we developed this site using WordPress and WooCommerce. All backend work was done in PHP and all styling was done using SASS. We used Asana to help manage our work load and keep us on pace to finish this project on time. This project displays my ability to work in a team environment.',
         tools: 'WordPress, WooCommerce, PHP, SASS, GitHub',
-        links: [<a href="https://github.com/" target='_blank' alt='github'>GitHub</a>, 
-                <a href="https://steliocodes.com/minesweeper/" target='_blank' alt='github'>Live Site</a>],
+        links: [<a href="https://github.com/" rel='noreffer noopener' target='_blank' alt='github'>GitHub</a>, 
+                <a href="https://steliocodes.com/minesweeper/" rel='noreffer noopener' target='_blank' alt='github'>Live Site</a>],
         slug: 'massagetherapy'
     },
     {
@@ -71,8 +71,8 @@ export const ProjectData = [
         image: require('../assets/images/port1.png'),
         summary: 'Built a portfolio website to show my work through out the last 6 months. I developed this website using react, sass, html, adobe XD and adobe illustrator. The wireframes were created in Adobe XD and so was the functional prototype while all images were taken from a free to use site and edited in Adobe illustrator. Created animation using Fader-Motion',
         tools: 'React, HMTL, SASS, GitHub',
-        links: [<a href="https://github.com/" target='_blank' alt='github'>GitHub</a>, 
-                <a href="https://steliocodes.com/minesweeper/" target='_blank' alt='github'>Live Site</a>],
+        links: [<a href="https://github.com/" rel='noreffer noopener' target='_blank' alt='github'>GitHub</a>, 
+                <a href="https://steliocodes.com/minesweeper/"rel='noreffer noopener' target='_blank' alt='github'>Live Site</a>],
         slug: 'portfolio'
     }
 ]
