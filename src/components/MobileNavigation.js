370743daa7c18947a5ca4b74bc437d2e097c3ddb
Stelianos Langaditis <slangaditis96@gmail.com>
2022-04-06 20:43:24 -0700

    nav working, changed contact page

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -1,38 +1,39 @@
 // import { useState } from 'react';
-import NavLinks from './NavLinks';
-import Footer from './Footer';
+// import NavLinks from './NavLinks';
+// import { useState } from 'react';
 
 
-const MobileNavigation = () => {
+// const MobileNavigation = () => {
 
-    // const [openHamburger, setHamburger] = useState(false);
-
-    // const showHamburger = () => setHamburger(!openHamburger)
+//     // const [ isOpen, setIsOpen ] = useState(false);
+    
+//     // function handleToggle() {
+//     //     setIsOpen(!isOpen);
+//     // };
     
 
-    return (
-        <nav className='mobile-nav'>
-            <div className="navbar">
-                <div className="container nav-container">
-                    <input 
-                    className="checkbox" 
-                    type="checkbox" 
-                    readOnly={true}
-                    id="burger" 
-                    alt="navigation burger"/>  
-                        <div className="hamburger-lines" >
-                            <span className="line line1"></span>
-                            <span className="line line2"></span>
-                            <span className="line line3"></span>
-                        </div> 
-                            <div className="menu-items" >
-                                <NavLinks />
-                            </div>
-                </div>
-            </div>
+//     return (
+//         <nav className='mobile-nav'>
+//             <div className="navbar">
+//                 <div className="container nav-container">
+//                     <input 
+//                     className="checkbox" 
+//                     type="checkbox" 
+//                     id="burger" 
+//                     alt="navigation burger"/>  
+//                         <div className="hamburger-lines">
+//                             <span className="line line1"></span>
+//                             <span className="line line2"></span>
+//                             <span className="line line3"></span>
+//                         </div> 
+//                             <div className="menu-items" >
+//                                 <NavLinks />
+//                             </div>
+//                 </div>
+//             </div>
        
-        </nav>
-    );
-}
+//         </nav>
+//     );
+// }
 
-export default MobileNavigation;
+// export default MobileNavigation;
---
+++ src/components/SidebarData.js
@@ -0,0 +1,41 @@
+// import React from 'react';
+// import * as AiIcons from "react-icons/ai";
+// import * as MdIcons from 'react-icons/md';
+
+export const SideBarData = [
+    {
+        title: 'Home',
+        path: '/',
+        cName: 'nav-text'
+    },
+    {
+        title: 'About',
+        path: '/about',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Work',
+        path: '/work',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Contact',
+        path: '/contact',
+        cName: 'nav-text'
+    },
+    // {
+    //     title: <a href="https://www.linkedin.com/in/stelio-langaditis"  rel='noreffer noopener' target='_blank' alt='linkedin'><AiIcons.AiFillLinkedin/></a>,
+    //     path:'#',
+    //     cName: 'nav-text'
+    // },
+    // {
+    //     title: <a href="https://www.github.com/Stelio123" rel='noreffer noopener' target='_blank' alt='github'><AiIcons.AiFillGithub/></a>,
+    //     path:'#' ,
+    //     cName: 'nav-text'
+    // },
+    // {   title: <a href="mailto:slangaditis96@gmail.com"  alt='email'><MdIcons.MdOutlineAlternateEmail/></a>,
+    //     path:'#',
+    //     cName: 'nav-text'
+    // },
+
+]
